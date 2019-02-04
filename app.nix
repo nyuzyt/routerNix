8e0647261338363183a833501bc02eab66470015
nyuzyt <jstzzyt01@gmail.com>
2019-02-03 22:49:54 -0500

    work on ubuntu

--- app.nix
+++ app.nix
@@ -1,18 +1,12 @@
-{ mkDerivation, aeson, attoparsec, base, base-compat, blaze-html
-, blaze-markup, bytestring, directory, http-media, http-types
-, lucid, miso, mtl, servant, servant-server, stdenv
-, string-conversions, time, wai, warp
-}:
+{ mkDerivation, base, miso, stdenv }:
 mkDerivation {
-  pname = "routerNix";
+  pname = "app";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [
-    aeson attoparsec base base-compat blaze-html blaze-markup
-    bytestring directory http-media http-types lucid miso mtl servant
-    servant-server string-conversions time wai warp
-  ];
-  license = stdenv.lib.licenses.bsd3;
+  executableHaskellDepends = [ base miso ];
+  description = "First miso app";
+  license = "unknown";
+  hydraPlatforms = stdenv.lib.platforms.none;
 }
