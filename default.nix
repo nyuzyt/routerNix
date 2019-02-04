b9ed64a0677cac1bcafe89b731503f30aeb97c23
nyuzyt <jstzzyt01@gmail.com>
2019-02-04 00:04:32 -0500

    success build and run miso

--- default.nix
+++ default.nix
@@ -1,17 +1,31 @@
 { pkgs ? import ((import <nixpkgs> {}).fetchFromGitHub {
 	owner = "NixOS";
 	repo = "nixpkgs";
-	rev = "a0aeb23";
-	sha256 = "04dgg0f2839c1kvlhc45hcksmjzr8a22q1bgfnrx71935ilxl33d";
+	rev = "09218fe5de4e85d182a1e55386996cd284ad4049";
+	sha256 = "1mmrnyp80jfvqdsal9728h8y3pmyx5snvgls0zfmbjl4qm3m8f76";
   }){}
 }:
 let
-  result = import (pkgs.fetchFromGitHub {
-	owner = "dmjio";
-	repo = "miso";
-	sha256 = "1l1gwzzqlvvcmg70jjrwc5ijv1vb6y5ljqkh7rxxq7hkyxpjyx9q";
-	rev = "95f6bc9b1ae6230b110358a82b6a573806f272c2";
-  }) {};
-in pkgs.haskell.packages.ghcjs.callPackage ./app.nix {
-  miso = result.miso-ghcjs;
+  inherit (pkgs) runCommand closurecompiler;
+    inherit (pkgs.haskell.packages) ghc844;
+    ghc = ghc844.override (oldAttrs: {
+      overrides = with pkgs.haskell.lib; self: super: {
+      };
+    });
+    ghcjs = pkgs.haskell.packages.ghcjs.override (oldAttrs: {
+      overrides = with pkgs.haskell.lib; self: super: {
+        jsaddle-warp = dontCheck (super.callPackage ./nix/jsaddle-warp-ghcjs.nix {});
+        miso = dontCheck super.miso;
+        aeson = dontCheck super.aeson;
+        http-types = dontCheck super.http-types;
+        doctest = dontCheck super.doctest;
+        tasty-quickcheck = dontCheck super.tasty-quickcheck;
+        scientific = dontCheck super.scientific;
+        directory-tree = dontCheck super.directory-tree;
+        webdriver = dontCheck super.webdriver;
+        servant = dontCheck super.servant;
+      };
+    });
+
+in ghcjs.callPackage ./app.nix {
 }
