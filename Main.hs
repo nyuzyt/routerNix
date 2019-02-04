71650cf6f2d4df0d8b8824aab364fbbc08480bb3
nyuzyt <jstzzyt01@gmail.com>
2019-02-03 21:46:54 -0500

    will test on ubuntu

--- Main.hs
+++ Main.hs
@@ -11,13 +11,65 @@
 -- ####################################
 
 -- wai example
+-- ####################################
+-- module Main where
+--
+-- import Network.Wai.Handler.Warp (run)
+--
+-- import YWai
+--
+-- main :: IO ()
+-- main = do
+--     putStrLn $ "http://localhost:8080/"
+--     run 8080 app
+-- ####################################
+
+-- | Haskell language pragma
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+-- | Haskell module declaration
 module Main where
 
-import Network.Wai.Handler.Warp (run)
+-- | Miso framework import
+import Miso
+import Miso.String
 
-import YWai
+-- | Type synonym for an application model
+type Model = Int
 
+-- | Sum type for application events
+data Action
+  = AddOne
+  | SubtractOne
+  | NoOp
+  | SayHelloWorld
+  deriving (Show, Eq)
+
+-- | Entry point for a miso application
 main :: IO ()
-main = do
-    putStrLn $ "http://localhost:8080/"
-    run 8080 app
+main = startApp App {..}
+  where
+    initialAction = SayHelloWorld -- initial action to be executed on application load
+    model  = 0                    -- initial model
+    update = updateModel          -- update function
+    view   = viewModel            -- view function
+    events = defaultEvents        -- default delegated events
+    subs   = []                   -- empty subscription list
+    mountPoint = Nothing          -- mount point for application (Nothing defaults to 'body')
+
+-- | Updates model, optionally introduces side effects
+updateModel :: Action -> Model -> Effect Action Model
+updateModel AddOne m = noEff (m + 1)
+updateModel SubtractOne m = noEff (m - 1)
+updateModel NoOp m = noEff m
+updateModel SayHelloWorld m = m <# do
+  putStrLn "Hello World" >> pure NoOp
+
+-- | Constructs a virtual DOM from a model
+viewModel :: Model -> View Action
+viewModel x = div_ [] [
+   button_ [ onClick AddOne ] [ text "+" ]
+ , text (ms x)
+ , button_ [ onClick SubtractOne ] [ text "-" ]
+ ]
