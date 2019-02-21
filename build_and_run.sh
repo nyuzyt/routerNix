0fcd7e85337d13d0cebbd3e9360b79dd2fc80400
nyuzyt <jstzzyt01@gmail.com>
2019-02-20 20:39:52 -0500

    first commit

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env sh
 
-# usage: ag -l | entr -r ./build_and_run.sh
-
 nix build
 status=$?
 if test "$status" -eq 0
 then
-    cd result && bin/app;
+    cd result && bin/server;
 else
     echo "Waiting for file changes..."
 fi
--- default.nix
+++ default.nix
@@ -1,31 +1,14 @@
-{ pkgs ? import ((import <nixpkgs> {}).fetchFromGitHub {
-	owner = "NixOS";
-	repo = "nixpkgs";
-	rev = "09218fe5de4e85d182a1e55386996cd284ad4049";
-	sha256 = "1mmrnyp80jfvqdsal9728h8y3pmyx5snvgls0zfmbjl4qm3m8f76";
-  }){}
-}:
 let
-  inherit (pkgs) runCommand closurecompiler;
-    inherit (pkgs.haskell.packages) ghc844;
-    ghc = ghc844.override (oldAttrs: {
-      overrides = with pkgs.haskell.lib; self: super: {
-      };
-    });
-    ghcjs = pkgs.haskell.packages.ghcjs.override (oldAttrs: {
-      overrides = with pkgs.haskell.lib; self: super: {
-        jsaddle-warp = dontCheck (super.callPackage ./nix/jsaddle-warp-ghcjs.nix {});
-        miso = dontCheck super.miso;
-        aeson = dontCheck super.aeson;
-        http-types = dontCheck super.http-types;
-        doctest = dontCheck super.doctest;
-        tasty-quickcheck = dontCheck super.tasty-quickcheck;
-        scientific = dontCheck super.scientific;
-        directory-tree = dontCheck super.directory-tree;
-        webdriver = dontCheck super.webdriver;
-        servant = dontCheck super.servant;
-      };
-    });
 
-in ghcjs.callPackage ./app.nix {
-}
+  pkgs = import ./nixpkgs.nix ;
+  server = pkgs.callPackage ./server/default.nix {};
+  client = pkgs.callPackage ./client/default.nix {};
+
+in
+
+  pkgs.runCommand "miso-ismorphic-example" { inherit client server; } ''
+    mkdir -p $out/{bin,static}
+    cp ${server}/bin/* $out/bin/
+    cp ${client}/static/* $out/static/
+  ''
+
